ed7afd69e126b44a35f679598b0376507634562c
JulienZammit <julien@julien>
2024-09-25 15:45:08 +0200

    Sauvegarde temporaire

--- tests/main_test.py
+++ tests/main_test.py
@@ -1,9 +1,9 @@
 import unittest
-from givememoneyjulien.main import give_me_money_julien
+from givememoneyjulienbis.main import give_me_money_julien_bis
 
 class MyTestCase(unittest.TestCase):
     def test_something(self):
-        compte_en_banque = give_me_money_julien(8, 10)
+        compte_en_banque = give_me_money_julien_bis(8, 10)
         self.assertEqual(compte_en_banque, 18)
         
 if __name__ == '__main__':
